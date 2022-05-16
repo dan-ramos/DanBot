c15336b570e4894c0b93ac6427f791ac4af01487
Daniel Ramos <ramosd675@gmail.com>
2022-05-16 17:10:44 -0500

    post changes in SA

--- commands/print.js
+++ commands/print.js
@@ -1,9 +1,30 @@
+const fs = require('fs');
 module.exports={
-    name: 'bruh',
-    description: '',
+    name: 'print',
+    description: 'saves message to printing queue',
     execute(message, args){
-        for(i in args){
-            message.channel.send(args[i]);
-        }
+        const yupEmoji = '👍';
+        message.react(yupEmoji);
+
+        let author = message.author.username;
+        let content = message.content.slice(7);
+        let msg = `${author}: ${content}`;
+
+        console.log(msg)
+
+        let path = __dirname + '/print/queue/' + Date.now() + '.txt';
+        fs.writeFile(path, msg, (err) => {
+            if(err){
+                console.log(err);
+                const sorryEmoji = '❌';
+                message.react(sorryEmoji);
+            }
+            else{
+                //console.log('wrote to ' + path);
+                const okEmoji = '🆗';
+                message.react(okEmoji);
+            }
+        });
+
     }
 }
